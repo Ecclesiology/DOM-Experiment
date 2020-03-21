e3aabcc08549d636d8a1b8a458d15a79b569a68a
Ecclesiology <JMMatthews@Protonmail.com>
2020-03-21 19:18:44 -0400

    Add third button

--- DOM JavaScript.js
+++ DOM JavaScript.js
@@ -42,3 +42,10 @@ const btn2 = document.querySelector('#btn2');
   btn2.addEventListener('click', () => {
     alert("Hello World");
   });
+  
+function alertFunction() {
+  alert("YAY! YOU DID IT!");
+}
+
+const btn3 = document.querySelector("#btn3");
+  btn3.addEventListener('click', alertFunction);
